406a529473003b1b762d476b66f55570002a1547
GabyD002 <andrei.dobrita@devnest.ro>
2022-11-21 11:27:33 +0200

    task_2

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Company;
 use App\Form\CompanyType;
 use Psr\Log\LoggerInterface;
