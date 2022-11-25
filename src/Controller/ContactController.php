afd695c80301d9c7bb5e9c69ec625d7b0c5690c3
GabyD002 <andrei.dobrita@devnest.ro>
2022-11-25 10:31:11 +0200

    task_4

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
-use App\Validation\DataValidatorInterface;
+use App\Service\DataValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
