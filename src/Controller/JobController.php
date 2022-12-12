43c1330bdb73672af4db8fecfe7d4e77f9d098a5
GabyD002 <andrei.dobrita@devnest.ro>
2022-12-12 05:35:37 +0200

    task_8

---
+++ src/Controller/JobController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JobController extends AbstractController
+{
+    #[Route('/job', name: 'app_job')]
+    public function index(): Response
+    {
+        return $this->render('job/index.html.twig', [
+            'controller_name' => 'JobController',
+        ]);
+    }
+}
